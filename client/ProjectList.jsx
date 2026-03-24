6a85772bc8f0cad63fb8cbaa9bf516e54779b00c
WilsonXia <97630511+WilsonXia@users.noreply.github.com>
2026-03-24 19:46:44 -0400

    tweaked the ProjectList.jsx styling

--- client/ProjectList.jsx
+++ client/ProjectList.jsx
@@ -11,13 +11,13 @@ const ProjectList = (props) => {
     const responsive = {
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 3 // optional, default to 1.
+            items: 3.8,
+            // slidesToSlide: 3 // optional, default to 1.
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
             items: 2,
-            slidesToSlide: 2 // optional, default to 1.
+            // slidesToSlide: 2 // optional, default to 1.
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
@@ -62,13 +62,17 @@ const ProjectList = (props) => {
                 triggerReload={props.triggerReload} />
         );
     });
-    console.log(Carousel);
 
     return (
         <Carousel 
             responsive={responsive}
+            // arrows={false}
+            // draggable={false}
+            containerClass='projectList'
         >
+            <div className='emptyTile'></div>
             {projectNodes}
+            <div></div>
         </Carousel>
     );
 }
